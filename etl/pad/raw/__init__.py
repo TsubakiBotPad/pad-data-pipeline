2e3f0bcd74ee74b8a6d28e51c7ce2f7acb5008a4
nachoapps <nachoappsinc@gmail.com>
2019-06-18 16:01:06 -0400

    Export more raw classes

--- etl/pad/raw/__init__.py
+++ etl/pad/raw/__init__.py
@@ -5,9 +5,15 @@ data structures.
 It should only depend on items from the common package.
 """
 
-from . import card
+from . import bonus, card, dungeon, enemy_skill, exchange, skill
 
+Bonus = bonus.Bonus
 Card = card.Card
 Curve = card.Curve
 Enemy = card.Enemy
 ESRef = card.ESRef
+Dungeon = dungeon.Dungeon
+DungeonFloor = dungeon.DungeonFloor
+MonsterSkill = skill.MonsterSkill
+Exchange = exchange.Exchange
+EnemySkill = enemy_skill.EnemySkill
