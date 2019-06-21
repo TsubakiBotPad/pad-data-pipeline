abe14a60e9ad6dbeb1fb008c3a58808884ea1236
nachoapps <nachoappsinc@gmail.com>
2019-06-21 12:42:35 -0400

    Cleaning naming, adding evolution support

--- etl/pad/storage_processor/monster_processor.py
+++ etl/pad/storage_processor/monster_processor.py
@@ -1,6 +1,6 @@
 from pad.db.db_util import DbWrapper
 from pad.raw_processor import crossed_data
-from pad.storage.monster import MonsterLS, MonsterAS, Monster, MonsterAwakening
+from pad.storage.monster import LeaderSkill, ActiveSkill, Monster, Awakenings, Evolution
 
 
 class MonsterProcessor(object):
@@ -9,8 +9,10 @@ class MonsterProcessor(object):
 
     def process(self, db: DbWrapper):
         self._process_skills(db)
+        print('loading', len(self.data.ownable_cards), 'cards')
         self._process_monsters(db)
         self._process_awakenings(db)
+        self._process_evolutions(db)
 
     def _process_skills(self, db: DbWrapper):
         skill_id_to_crossed_skills = {css.skill_id: css for css in self.data.skills}
@@ -33,23 +35,30 @@ class MonsterProcessor(object):
         print('loading', len(ls_in_use), 'ls in use')
         for ls_skill_id in ls_in_use:
             ls_css = skill_id_to_crossed_skills[ls_skill_id]
-            ls_item = MonsterLS.from_css(ls_css)
+            ls_item = LeaderSkill.from_css(ls_css)
             db.insert_or_update(ls_item)
 
         print('loading', len(as_in_use), 'as in use')
         for as_skill_id in as_in_use:
             as_css = skill_id_to_crossed_skills[as_skill_id]
-            as_item = MonsterAS.from_css(as_css)
+            as_item = ActiveSkill.from_css(as_css)
             db.insert_or_update(as_item)
 
     def _process_monsters(self, db):
-        print('loading', len(self.data.ownable_cards), 'cards')
         for m in self.data.ownable_cards:
             item = Monster.from_csm(m)
             db.insert_or_update(item)
 
     def _process_awakenings(self, db):
+        print('loading awakenings')
         for m in self.data.ownable_cards:
-            items = MonsterAwakening.from_csm(m)
+            items = Awakenings.from_csm(m)
             for item in items:
+                db.insert_or_update(item)
+
+    def _process_evolutions(self, db):
+        print('loading evolutions')
+        for m in self.data.ownable_cards:
+            item = Evolution.from_csm(m)
+            if item:
                 db.insert_or_update(item)
