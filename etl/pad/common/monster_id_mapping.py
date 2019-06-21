c9188e3233c96bc31a320be956a916e61d1f133f
nachoapps <nachoappsinc@gmail.com>
2019-06-21 12:42:11 -0400

    Cleaning up typing

--- etl/pad/common/monster_id_mapping.py
+++ etl/pad/common/monster_id_mapping.py
@@ -1,26 +1,23 @@
 # Pad NA/JP don't have the exact same monster IDs for the same monster.
 # We use JP ids as the monster number; NA-only cards are adjusted to a new range.
+from pad.common.shared_types import MonsterNo, CardId
 
 
 def between(n: int, bottom: int, top: int):
     return bottom <= n <= top
 
 
-def adjust(n: int, local_bottom: int, remote_bottom: int):
-    return n - local_bottom + remote_bottom
+def adjust(n: CardId, local_bottom: int, remote_bottom: int) -> MonsterNo:
+    return MonsterNo(n - local_bottom + remote_bottom)
 
 
-def jp_id_to_monster_no(jp_id: int):
-    jp_id = int(jp_id)
-
+def jp_id_to_monster_no(jp_id: CardId) -> MonsterNo:
     # We use JP IDs as the monster_no; no need to adjust.
-    return jp_id
+    return MonsterNo(jp_id)
 
 
 # Fixes for early collabs, and adjusting voltron to a new range.
-def nakr_id_to_monster_no(na_id: int):
-    na_id = int(na_id)
-
+def nakr_id_to_monster_no(na_id: CardId) -> MonsterNo:
     # Shinra Bansho 1
     if between(na_id, 934, 935):
         return adjust(na_id, 934, 669)
@@ -41,4 +38,4 @@ def nakr_id_to_monster_no(na_id: int):
     if between(na_id, 2601, 2631):
         return adjust(na_id, 2601, 2601 + 10000)
 
-    return na_id
+    return MonsterNo(na_id)
