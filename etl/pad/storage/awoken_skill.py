867caee0ab0ca764fa28dc7bac33784646a56854
nachoapps <nachoappsinc@gmail.com>
2019-06-21 12:40:32 -0400

    Adjsuting naming

--- etl/pad/storage/awoken_skill.py
+++ etl/pad/storage/awoken_skill.py
@@ -1,26 +1,26 @@
 from pad.db.sql_item import SimpleSqlItem
 
 
-class Awakening(SimpleSqlItem):
+class AwokenSkill(SimpleSqlItem):
     """Monster awakening."""
-    TABLE = 'awakening'
-    KEY_COL = 'awakening_id'
+    TABLE = 'awoken_skills'
+    KEY_COL = 'awoken_skill_id'
 
     @staticmethod
     def from_json(o):
-        return Awakening(awakening_id=o['pad_awakening_id'],
-                         name_jp=o['name_jp'],
-                         name_na=o['name_na'],
-                         name_kr=o['name_kr'],
-                         desc_jp=o['desc_jp'],
-                         desc_na=o['desc_na'],
-                         desc_kr=o['desc_kr'],
-                         adj_hp=o['adj_hp'],
-                         adj_atk=o['adj_atk'],
-                         adj_rcv=o['adj_rcv'])
+        return AwokenSkill(awoken_skill_id=o['pad_awakening_id'],
+                           name_jp=o['name_jp'],
+                           name_na=o['name_na'],
+                           name_kr=o['name_kr'],
+                           desc_jp=o['desc_jp'],
+                           desc_na=o['desc_na'],
+                           desc_kr=o['desc_kr'],
+                           adj_hp=o['adj_hp'],
+                           adj_atk=o['adj_atk'],
+                           adj_rcv=o['adj_rcv'])
 
     def __init__(self,
-                 awakening_id: int = None,
+                 awoken_skill_id: int = None,
                  name_jp='',
                  name_na: str = '',
                  name_kr: str = '',
@@ -31,7 +31,7 @@ class Awakening(SimpleSqlItem):
                  adj_atk: int = 0,
                  adj_rcv: int = 0,
                  tstamp: int = None):
-        self.awakening_id = awakening_id
+        self.awoken_skill_id = awoken_skill_id
         self.name_jp = name_jp
         self.name_na = name_na
         self.name_kr = name_kr
