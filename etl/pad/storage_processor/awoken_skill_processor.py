78c6b00fc0e5f4bf96e2f67eda48e33aa60265b6
nachoapps <nachoappsinc@gmail.com>
2019-06-21 18:46:49 -0400

    Correctly reference json file in cur dir

--- etl/pad/storage_processor/awoken_skill_processor.py
+++ etl/pad/storage_processor/awoken_skill_processor.py
@@ -1,12 +1,14 @@
 import json
+import os
 
 from pad.db.db_util import DbWrapper
 from pad.storage.awoken_skill import AwokenSkill
 
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 class AwakeningProcessor(object):
     def __init__(self):
-        with open('awoken_skill.json') as f:
+        with open(os.path.join(__location__, 'awoken_skill.json')) as f:
             self.awoken_skills = json.load(f)
 
     def process(self, db: DbWrapper):
