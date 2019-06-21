e59b9517f253392ecad5c6ee19d2ce7e1043c1c1
nachoapps <nachoappsinc@gmail.com>
2019-06-21 11:45:18 -0400

    Adding a utility for loading the dimension tables

---
+++ etl/pad/storage_processor/dimension_processor.py
@@ -0,0 +1,63 @@
+"""
+This processor sets up dimension tables that are used as foreign keys as other tables.
+In general it should do no work; it just simplifies the setup for a new database.
+"""
+from pad.db.db_util import DbWrapper
+
+D_ATTRIBUTE = {
+    'name': 'd_attribute',
+    'cols': ['attribute_id', 'name'],
+    'rows': [
+        [0, 'Red'],
+        [1, 'Blue'],
+        [2, 'Green'],
+        [3, 'Light'],
+        [4, 'Dark'],
+    ],
+}
+
+# Do we need this?
+D_CONDITION = {
+    'name': 'd_condition',
+    'cols': ['condition_type', 'name'],
+    'rows': [
+    ],
+}
+
+# Do we need this?
+D_TYPE = {
+    'name': 'd_type',
+    'cols': ['type_id', 'name'],
+    'rows': [
+            [0, 'Evolve'],
+            [1, 'Balance'],
+            [2, 'Physical'],
+            [3, 'Healer'],
+            [4, 'Dragon'],
+            [5, 'God'],
+            [6, 'Attacker'],
+            [7, 'Devil'],
+            [8, 'Machine'],
+            [12, 'Awoken'],
+            [14, 'Enhance'],
+            [15, 'Vendor'],
+    ],
+}
+
+DIMENSION_TABLES = [D_ATTRIBUTE, D_CONDITION, D_TYPE]
+
+
+class DimensionProcessor(object):
+    def __init__(self):
+        pass
+
+    def process(self, db: DbWrapper):
+        print('loading', len(DIMENSION_TABLES), 'dimension tables')
+        for tbl in DIMENSION_TABLES:
+            sql = 'INSERT IGNORE INTO {} ({}) VALUES '.format(tbl['name'], ','.join(tbl['cols']))
+            rows = []
+            for row in tbl['rows']:
+                row = list(map(lambda x: "'{}'".format(x), row))
+                rows.append('({})'.format(','.join(row)))
+            sql += ','.join(rows)
+            db.insert_item(sql)
