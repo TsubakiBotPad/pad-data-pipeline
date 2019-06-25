1863f0390124fd9a033745aee746c4ab35408c0c
nachoapps <nachoappsinc@gmail.com>
2019-06-25 16:30:03 -0400

    Fix table name parsing

--- web/serve.php
+++ web/serve.php
@@ -3,7 +3,9 @@
   $script = "${dg_home}/dadguide-data/web/serve_dadguide_data.py";
   $db_config = "${dg_home}/dadguide-jobs/db_config.json";
 
-  $cmd = "python3 ${script} --db_config=${db_config} --db_table=${tbl_name}";
+  $table = $_POST["table"];
+
+  $cmd = "python3 ${script} --db_config=${db_config} --db_table=${$table}";
   if (array_key_exists("tstamp", $_POST)) {
     $tstamp = $_POST["tstamp"];
     $cmd = "$cmd --tstamp=${tstamp}";
