e5d84c0ee0e1ea8611be6934d4ad3c99a6467d39
nachoapps <nachoappsinc@gmail.com>
2019-06-28 22:09:34 -0400

    Add sanic webserver for serving data

---
+++ web/data/utils.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from decimal import Decimal
+
+import pymysql
+
+
+def fix_row(row):
+    row_data = {}
+    for col in row:
+        data = row[col]
+        if data is None:
+            fixed_data = None
+        elif type(data) is Decimal:
+            first = '{}'.format(float(data))
+            second = '{:.1f}'.format(float(data))
+            fixed_data = max((first, second), key=len)
+        elif type(data) is datetime:
+            fixed_data = data.isoformat(' ')
+        else:
+            fixed_data = str(data)
+
+        fixed_col = col
+        row_data[fixed_col] = fixed_data
+    return row_data
+
+def dump_table(table_name, cursor):
+    result_json = {'items': []}
+    for row in cursor:
+        result_json['items'].append(fix_row(row))
+
+    return result_json
+
+
+def load_from_db(db_config, table, tstamp):
+    connection = pymysql.connect(host=db_config['host'],
+                                 user=db_config['user'],
+                                 password=db_config['password'],
+                                 db=db_config['db'],
+                                 charset=db_config['charset'],
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    table = table.lower()
+    sql = 'SELECT * FROM `{}`'.format(table)
+
+    if table == 'timestamps':
+        pass
+    else:
+        sql += ' WHERE tstamp > {}'.format(int(tstamp))
+
+        if table == 'schedule':
+            sql += ' AND close_timestamp > UNIX_TIMESTAMP()'
+
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        data = dump_table(table, cursor)
+
+    connection.close()
+    return data
---
+++ web/server.py
@@ -0,0 +1,75 @@
+import argparse
+
+from sanic import Sanic
+from sanic.exceptions import ServerError
+from sanic.response import json
+
+import json as base_json
+
+from sanic_compress import Compress
+
+from data.utils import load_from_db
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="DadGuide backend server", add_help=False)
+    input_group = parser.add_argument_group("Input")
+    input_group.add_argument("--db_config", help="JSON database info")
+    return parser.parse_args()
+
+
+app = Sanic()
+Compress(app)
+
+db_config = None
+
+VALID_TABLES = [
+    'active_skills',
+    'awakenings',
+    'awoken_skills',
+    'd_attributes',
+    'd_event_types',
+    'drops',
+    'dungeons',
+    'dungeon_type',
+    'encounters',
+    'evolutions',
+    'leader_skills',
+    'monsters',
+    'news',
+    'rank_rewards',
+    'schedule',
+    'series',
+    'skill_condition',
+    'sub_dungeons',
+    'timestamps'
+]
+
+
+@app.route('/serve-table')
+async def serve_table(request):
+    table = request.args.get('table')
+    if table is None:
+        raise ServerError('table required')
+    if table not in VALID_TABLES:
+        raise ServerError('unexpected table')
+    tstamp = request.args.get('tstamp')
+    if tstamp is None and table != 'timestamps':
+        raise ServerError('tstamp required')
+    if tstamp is not None and not tstamp.isnumeric():
+        raise ServerError('tstamp must be a number')
+
+    data = load_from_db(db_config, table, tstamp)
+    return json(data)
+
+
+def main(args):
+    with open(args.db_config) as f:
+        global db_config
+        db_config = base_json.load(f)
+
+    app.run(host='0.0.0.0', port=8000)
+
+
+if __name__ == '__main__':
+    main(parse_args())
