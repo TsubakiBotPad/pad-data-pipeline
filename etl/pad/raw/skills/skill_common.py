700578149313bfe46f03dae2003f32001523bd15
nachoapps <nachoapps@gmail.com>
2019-10-20 21:56:37 -0400

    First example of i13n changes

--- etl/pad/raw/skills/skill_common.py
+++ etl/pad/raw/skills/skill_common.py
@@ -1,4 +1,9 @@
 from operator import itemgetter
+from typing import Dict, List
+
+
+class I13NotImplemented(Exception):
+    pass
 
 
 def fmt_mult(x):
@@ -15,106 +20,49 @@ def multi_getattr(o, *args):
 
 class BaseTextConverter(object):
     """Contains code shared across AS and LS converters."""
-    ATTRIBUTES = {0: 'Fire',
-                  1: 'Water',
-                  2: 'Wood',
-                  3: 'Light',
-                  4: 'Dark',
-                  5: 'Heal',
-                  6: 'Jammer',
-                  7: 'Poison',
-                  8: 'Mortal Poison',
-                  9: 'Bomb'}
 
-    TYPES = {0: 'Evo Material',
-             1: 'Balanced',
-             2: 'Physical',
-             3: 'Healer',
-             4: 'Dragon',
-             5: 'God',
-             6: 'Attacker',
-             7: 'Devil',
-             8: 'Machine',
-             12: 'Awaken Material',
-             14: 'Enhance Material',
-             15: 'Redeemable Material'}
+    #############################################################################
+    # Items here must be implemented by language-specific subclasses
+    #############################################################################
 
-    # TODO: we have these in awoken_skill.json should load from there. maybe do the same for the others
-    AWAKENING_MAP = {
-        0: '',  # No need.
-        1: 'Enhanced HP',
-        2: 'Enhanced Attack',
-        3: 'Enhanced Heal',
-        4: 'Reduce Fire Damage',
-        5: 'Reduce Water Damage',
-        6: 'Reduce Wood Damage',
-        7: 'Reduce Light Damage',
-        8: 'Reduce Dark Damage',
-        9: 'Auto-Recover',
-        10: 'Resistance-Bind',
-        11: 'Resistance-Dark',
-        12: 'Resistance-Jammers',
-        13: 'Resistance-Poison',
-        14: 'Enhanced Fire Orbs',
-        15: 'Enhanced Water Orbs',
-        16: 'Enhanced Wood Orbs',
-        17: 'Enhanced Light Orbs',
-        18: 'Enhanced Dark Orbs',
-        19: 'Extend Time',
-        20: 'Recover Bind',
-        21: 'Skill Boost',
-        22: 'Enhanced Fire Att.',
-        23: 'Enhanced Water Att.',
-        24: 'Enhanced Wood Att.',
-        25: 'Enhanced Light Att.',
-        26: 'Enhanced Dark Att.',
-        27: 'Two-Pronged Attack',
-        28: 'Resistance-Skill Bind',
-        29: 'Enhanced Heal Orbs',
-        30: 'Multi Boost',
-        31: 'Dragon Killer',
-        32: 'God Killer',
-        33: 'Devil Killer',
-        34: 'Machine Killer',
-        35: 'Balanced Killer',
-        36: 'Attacker Killer',
-        37: 'Physical Killer',
-        38: 'Healer Killer',
-        39: 'Evolve Material Killer',
-        40: 'Awaken Material Killer',
-        41: 'Enhance Material Killer',
-        42: 'Vendor Material Killer',
-        43: 'Enhanced Combo',
-        44: 'Guard Break',
-        45: 'Additional Attack',
-        46: 'Enhanced Team HP',
-        47: 'Enhanced Team RCV',
-        48: 'Damage Void Shield Penetration',
-        49: 'Awoken Assist',
-        50: 'Super Additional Attack',
-        51: 'Skill Charge',
-        52: 'Resistance-Bind＋',
-        54: 'Resistance-Cloud',
-        53: 'Extend Time＋',
-        55: 'Resistance-Board Restrict',
-        56: 'Skill Boost＋',
-        57: 'Enhance when HP is above 80%',
-        58: 'Enhance when HP is below 50%',
-        59: 'L-Shape Damage Reduction',
-        60: 'L-Shape Attack',
-        61: 'Super Enhanced Combo',
-        62: 'Combo Orb',
-        63: 'Skill Voice',
-        64: 'Dungeon Bonus',
-        65: 'Reduced HP',
-        66: 'Reduced Attack',
-        67: 'Reduced RCV',
-    }
+    @property
+    def ATTRIBUTES(self) -> Dict[int, str]:
+        raise I13NotImplemented()
 
-    def attributes_format(self, attributes):
+    @property
+    def TYPES(self) -> Dict[int, str]:
+        raise I13NotImplemented()
+
+    @property
+    def AWAKENING_MAP(self) -> Dict[int, str]:
+        raise I13NotImplemented()
+
+    def all_stats(self, multiplier):
+        raise I13NotImplemented()
+
+    def hp(self):
+        raise I13NotImplemented()
+
+    def atk(self):
+        raise I13NotImplemented()
+
+    def rcv(self):
+        raise I13NotImplemented()
+
+    def reduce_all_pct(self, shield_text):
+        raise I13NotImplemented()
+
+    def reduce_attr_pct(self, attr_text, shield_text):
+        raise I13NotImplemented()
+
+    #############################################################################
+    # Everything below here are common helpers
+    #############################################################################
+
+    def attributes_format(self, attributes: List[int]) -> str:
         return ', '.join([self.ATTRIBUTES[i] for i in attributes])
 
-    def types_format(self, types):
+    def types_format(self, types: List[int]) -> str:
         return ', '.join([self.TYPES[i] for i in types])
 
     def fmt_stats_type_attr_bonus(self, ls,
@@ -140,7 +88,7 @@ class BaseTextConverter(object):
 
             for_skill_text = ''
             if types:
-                for_skill_text += ' ' + ', '.join([self.TYPES[i] for i in types]) + ' type'
+                for_skill_text += ' {} type'.format(self.types_format(types))
 
             is_attr_all = len(attributes) in [0, 5]
             should_skip_attr = is_attr_all and skip_attr_all
@@ -186,9 +134,9 @@ class BaseTextConverter(object):
         if hp_mult == atk_mult and atk_mult == rcv_mult:
             if hp_mult == 1:
                 return None
-            return '{}x all stats'.format(fmt_mult(hp_mult))
+            return self.all_stats(fmt_mult(hp_mult))
 
-        mults = [('HP', hp_mult), ('ATK', atk_mult), ('RCV', rcv_mult)]
+        mults = [(self.hp(), hp_mult), (self.atk(), atk_mult), (self.rcv(), rcv_mult)]
         mults = list(filter(lambda x: x[1] != 1, mults))
         mults.sort(key=itemgetter(1), reverse=True)
 
@@ -214,9 +162,9 @@ class BaseTextConverter(object):
     def fmt_reduct_text(self, shield, reduct_att=[0, 1, 2, 3, 4]):
         if shield == 0:
             return None
+        shield_text = fmt_mult(shield * 100)
         if reduct_att == [0, 1, 2, 3, 4] or not reduct_att:
-            return 'reduce damage taken by {}%'.format(fmt_mult(shield * 100))
+            return self.reduce_all_pct(shield_text)
         else:
             color_text = self.attributes_format(reduct_att)
-            return 'reduce damage taken from ' + color_text + \
-                   ' Att. by {}%'.format(fmt_mult(shield * 100))
+            return self.reduce_attr_pct(color_text, shield_text)
