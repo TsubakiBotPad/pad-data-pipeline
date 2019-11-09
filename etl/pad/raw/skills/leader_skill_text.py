72c477e39380190f176304a78e84a98eecaade8e
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2019-11-09 11:26:58 -0600

    collab_bonus_convert to en_leader_skill_text.py

--- etl/pad/raw/skills/leader_skill_text.py
+++ etl/pad/raw/skills/leader_skill_text.py
@@ -1,5 +1,5 @@
 from pad.raw.skills.leader_skill_common import ThresholdType, Tag, AttributeDict
-from pad.raw.skills.skill_common import BaseTextConverter, fmt_mult, multi_getattr
+from pad.raw.skills.skill_common import BaseTextConverter, fmt_mult, multi_getattr, I13NotImplemented
 
 
 class LsTextConverter(BaseTextConverter):
@@ -348,92 +348,7 @@ class LsTextConverter(BaseTextConverter):
         return skill_text
 
     def collab_bonus_convert(self, ls):
-        COLLAB_MAP = {
-            0: '',
-            1: 'Ragnarok Online Collab',
-            2: 'Taiko no Tatsujin Collab',
-            3: 'ECO Collab',
-            5: 'Gunma\'s Ambition Collab',
-            6: 'Final Fantasy Crystal Defender Collab',
-            7: 'Famitsu Collab',
-            8: 'Princess Punt Sweet Collab',
-            9: 'Android Collab',
-            10: 'Batman Collab',
-            11: 'Capybara-san Collab',
-            12: 'GungHo Collab',
-            13: 'GungHo Collab',
-            14: 'Evangelion Collab',
-            15: 'Seven Eleven Collab',
-            16: 'Clash of Clan Collab',
-            17: 'Groove Coaster Collab',
-            18: 'RO ACE Collab',
-            19: 'Dragon\'s Dogma Collab',
-            20: 'Takaoka City Collab',
-            21: 'Monster Hunter 4G Collab',
-            22: 'Shinrabansho Choco Collab',
-            23: 'Thirty One Icecream Collab',
-            24: 'Angry Bird Collab',
-            26: 'Hunter x Hunter Collab',
-            27: 'Hello Kitty Collab',
-            28: 'PAD Battle Tournament Collab',
-            29: 'BEAMS Collab',
-            30: 'Dragon Ball Z Collab',
-            31: 'Saint Seiya Collab',
-            32: 'GungHo Collab',
-            33: 'GungHo Collab',
-            34: 'GungHo Collab',
-            35: 'Gungho Collab',
-            36: 'Bikkuriman Collab',
-            37: 'Angry Birds Collab',
-            38: 'DC Universe Collab',
-            39: 'Sangoku Tenka Trigger Collab',
-            40: 'Fist of the North Star Collab',
-            41: 'Chibi Series',
-            44: 'Chibi Keychain Series',
-            45: 'Final Fantasy Collab',
-            46: 'Ghost in Shell Collab',
-            47: 'Duel Masters Collab',
-            48: 'Attack on Titans Collab',
-            49: 'Ninja Hattori Collab',
-            50: 'Shounen Sunday Collab',
-            51: 'Crows Collab',
-            52: 'Bleach Collab',
-            53: 'DC Universe Collab',
-            55: 'Ace Attorney Collab',
-            56: 'Kenshin Collab',
-            57: 'Pepper Collab',
-            58: 'Kinnikuman Collab',
-            59: 'Napping Princess Collab',
-            60: 'Magazine All-Stars Collab',
-            61: 'Monster Hunter Collab',
-            62: 'Special edition MP series',
-            64: 'DC Universe Collab',
-            65: 'Full Metal Alchemist Collab',
-            66: 'King of Fighters \'98 Collab',
-            67: 'Yu Yu Hakusho Collab',
-            68: 'Persona Collab',
-            69: 'Coca Cola Collab',
-            70: 'Magic: The Gathering Collab',
-            71: 'GungHo Collab',
-            72: 'GungHo Collab',
-            74: 'Power Pro Collab',
-            76: 'Sword Art Online Collab',
-            77: 'Kamen Rider Collab',
-            78: 'Yo-kai Watch World Collab',
-            83: 'Shaman King Collab',
-            85: 'Samurai Spirits',
-            86: 'Power Rangers',
-            10001: 'Dragonbounds & Dragon Callers',
-        }
-
-        collab_id = ls.collab_id
-        if collab_id not in COLLAB_MAP:
-            print('Missing collab name for', collab_id)
-
-        collab_name = COLLAB_MAP.get(collab_id, '<not populated:{}>'.format(collab_id))
-        skill_text = self.fmt_stats_type_attr_bonus(ls) + ' when all cards are from ' + collab_name
-
-        return skill_text
+        raise I13NotImplemented()
 
     def multi_mass_match_convert(self, ls):
         if ls.atk not in [0, 1]:
