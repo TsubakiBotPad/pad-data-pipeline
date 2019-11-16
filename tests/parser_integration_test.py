63ac23ad74150069701cae8da5129248e75f41b4
nachoapps <nachoappsinc@gmail.com>
2019-11-16 09:41:24 -0500

    Fix accidental commit

--- tests/parser_integration_test.py
+++ tests/parser_integration_test.py
@@ -64,10 +64,10 @@ def run_test(args):
 
     # Add sort by something, bonuses seem to be non-deterministicly ordered.
     files = {
-        # 'all_cards.json': cross_db.all_cards,
-        # 'dungeons.json': cross_db.dungeons,
-        # 'active_skills.json': cross_db.active_skills,
-        # 'leader_skills.json': cross_db.leader_skills,
+        'all_cards.json': cross_db.all_cards,
+        'dungeons.json': cross_db.dungeons,
+        'active_skills.json': cross_db.active_skills,
+        'leader_skills.json': cross_db.leader_skills,
         'enemy_skills.json': cross_db.enemy_skills,
         # 'jp_bonuses.json': cross_db.jp_bonuses,
         # 'na_bonuses.json': cross_db.na_bonuses,
