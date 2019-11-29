d0085cf815147e3b26aea56fceafbd7c093dcf90
nachoapps <nachoappsinc@gmail.com>
2019-11-28 22:21:53 -0500

    Fixing log messages

--- etl/pad/raw_processor/merged_database.py
+++ etl/pad/raw_processor/merged_database.py
@@ -53,14 +53,14 @@ def _clean_cards(server: Server,
         if card.active_skill_id:
             active_skill = db.active_skill_by_id(card.active_skill_id)
             if active_skill is None:
-                fail_logger.warning('Active skill lookup failed: %s - %s'.format(
-                    repr(card), card.active_skill_id))
+                fail_logger.warning('Active skill lookup failed: %s - %s',
+                                    repr(card), card.active_skill_id)
 
         if card.leader_skill_id:
             leader_skill = db.leader_skill_by_id(card.leader_skill_id)
             if leader_skill is None:
-                fail_logger.warning('Leader skill lookup failed: %s - %s'.format(
-                    repr(card), card.leader_skill_id))
+                fail_logger.warning('Leader skill lookup failed: %s - %s',
+                                    repr(card), card.leader_skill_id)
 
         enemy = enemy_by_enemy_id.get(card.monster_no)
         enemy_skills = enemy.enemy_skills if enemy else []
