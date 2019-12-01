9c349d5fc13a209e7fb9be31f91de2bed5a098ce
nachoapps <nachoappsinc@gmail.com>
2019-12-01 17:45:09 -0500

    Sort imports

--- etl/pad/storage/monster.py
+++ etl/pad/storage/monster.py
@@ -5,10 +5,6 @@ from pad.common.shared_types import Server, MonsterId, MonsterNo, EvolutionType
 from pad.db import sql_item
 from pad.db.sql_item import SimpleSqlItem, ExistsStrategy
 from pad.raw.skills import skill_text_typing
-from pad.raw.skills.active_skill_info import ActiveSkill as RawActiveSkill
-from pad.raw.skills.en_active_skill_text import EnAsTextConverter
-from pad.raw.skills.en_leader_skill_text import EnLsTextConverter
-from pad.raw.skills.leader_skill_info import LeaderSkill as RawLeaderSkill
 from pad.raw_processor.crossed_data import CrossServerCard, CrossServerSkill
 from pad.storage.series import Series
 
