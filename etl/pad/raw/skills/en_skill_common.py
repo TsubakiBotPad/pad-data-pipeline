f60951f7434af75db333559c8a90aa32f8a47e2b
nachoapps <nachoappsinc@gmail.com>
2019-12-31 14:01:22 -0500

    Manually merging change from AA: https://github.com/nachoapps/dadguide-data/pull/29
    Moved ES descriptions to the describe object, added 7x6 ES, cleaned up pluralizations, start using awakening skill json instead of hardcoded list.

--- etl/pad/raw/skills/en_skill_common.py
+++ etl/pad/raw/skills/en_skill_common.py
@@ -1,6 +1,10 @@
+import json, os
 from typing import Dict
 
 from pad.raw.skills.skill_common import BaseTextConverter
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+AWOSKILLS = json.load(open(os.path.join(__location__, "../../storage_processor/awoken_skill.json")))
 
 
 class EnBaseTextConverter(BaseTextConverter):
@@ -30,77 +34,8 @@ class EnBaseTextConverter(BaseTextConverter):
               14: 'Enhance Material',
               15: 'Redeemable Material'}
 
-    # TODO: we have these in awoken_skill.json should load from there. maybe do the same for the others
-    _AWAKENING_MAP = {
-        0: '',  # No need.
-        1: 'Enhanced HP',
-        2: 'Enhanced Attack',
-        3: 'Enhanced Heal',
-        4: 'Reduce Fire Damage',
-        5: 'Reduce Water Damage',
-        6: 'Reduce Wood Damage',
-        7: 'Reduce Light Damage',
-        8: 'Reduce Dark Damage',
-        9: 'Auto-Recover',
-        10: 'Resistance-Bind',
-        11: 'Resistance-Dark',
-        12: 'Resistance-Jammers',
-        13: 'Resistance-Poison',
-        14: 'Enhanced Fire Orbs',
-        15: 'Enhanced Water Orbs',
-        16: 'Enhanced Wood Orbs',
-        17: 'Enhanced Light Orbs',
-        18: 'Enhanced Dark Orbs',
-        19: 'Extend Time',
-        20: 'Recover Bind',
-        21: 'Skill Boost',
-        22: 'Enhanced Fire Att.',
-        23: 'Enhanced Water Att.',
-        24: 'Enhanced Wood Att.',
-        25: 'Enhanced Light Att.',
-        26: 'Enhanced Dark Att.',
-        27: 'Two-Pronged Attack',
-        28: 'Resistance-Skill Bind',
-        29: 'Enhanced Heal Orbs',
-        30: 'Multi Boost',
-        31: 'Dragon Killer',
-        32: 'God Killer',
-        33: 'Devil Killer',
-        34: 'Machine Killer',
-        35: 'Balanced Killer',
-        36: 'Attacker Killer',
-        37: 'Physical Killer',
-        38: 'Healer Killer',
-        39: 'Evolve Material Killer',
-        40: 'Awaken Material Killer',
-        41: 'Enhance Material Killer',
-        42: 'Vendor Material Killer',
-        43: 'Enhanced Combo',
-        44: 'Guard Break',
-        45: 'Additional Attack',
-        46: 'Enhanced Team HP',
-        47: 'Enhanced Team RCV',
-        48: 'Damage Void Shield Penetration',
-        49: 'Awoken Assist',
-        50: 'Super Additional Attack',
-        51: 'Skill Charge',
-        52: 'Resistance-Bind＋',
-        54: 'Resistance-Cloud',
-        53: 'Extend Time＋',
-        55: 'Resistance-Board Restrict',
-        56: 'Skill Boost＋',
-        57: 'Enhance when HP is above 80%',
-        58: 'Enhance when HP is below 50%',
-        59: 'L-Shape Damage Reduction',
-        60: 'L-Shape Attack',
-        61: 'Super Enhanced Combo',
-        62: 'Combo Orb',
-        63: 'Skill Voice',
-        64: 'Dungeon Bonus',
-        65: 'Reduced HP',
-        66: 'Reduced Attack',
-        67: 'Reduced RCV',
-    }
+    _AWAKENING_MAP = {x['pad_awakening_id']: x['name_na'] for x in AWOSKILLS}
+    _AWAKENING_MAP[0] = ''
 
     @property
     def ATTRIBUTES(self) -> Dict[int, str]:
