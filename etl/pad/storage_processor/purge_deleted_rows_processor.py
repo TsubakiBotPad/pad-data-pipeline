b6f0770791c55b576ea1d2bd07de8f48c5414da4
Chase Hult <chasehult@gmail.com>
2019-12-31 04:08:08 -0800

    TODO #7
    I’m not a server person.  I think I did this right, but I’m not sure.
    I may have completely misunderstood everything

---
+++ etl/pad/storage_processor/purge_deleted_rows_processor.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import datetime
+
+from pad.db.db_util import DbWrapper
+from pad.common.shared_types import Server
+from pad.storage.exchange import Exchange
+from pad.raw_processor import crossed_data
+ 
+logger = logging.getLogger('processor')
+
+def date2tstamp(date):
+    return int(date.timestamp())
+
+class PurgeDeletedRowProcessor(object):
+    def __init__(self):
+        pass
+
+    def process(self, db: DbWrapper):
+        db.fetch_data("DELETE FROM `deleted_rows` WHERE tstamp < {}" \
+                      .format(date2tstamp(datetime.now()-timedelta(1))))
+
