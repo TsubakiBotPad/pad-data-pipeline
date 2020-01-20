a4273ee1c6e6a00586701a4d33f5e2f2cc5447aa
Chase Hult <chasehult@gmail.com>
2020-01-19 22:46:31 -0800

    fat move

--- etl/pad/raw/skills/jp/skill_common.py
+++ etl/pad/raw/skills/jp/skill_common.py
@@ -124,3 +124,117 @@ class JpBaseTextConverter(BaseTextConverter):
             return str(n)[:-4] + '万' + str(n)[-4:]
         else:
             return str(n)[:-8] + '億' + str(n)[-8:-4] + '万' + str(n)[-4:]
+
+    ################################################
+    #               Format Functions               #
+    ################################################
+    
+    def attributes_format(self, attributes: List[int], sep: str = ', ') -> str:
+        return sep.join([self.ATTRIBUTES[i] for i in attributes])
+
+    def types_format(self, types: List[int]) -> str:
+        return ', '.join([self.TYPES[i] for i in types])
+
+    def fmt_stats_type_attr_bonus(self, ls,
+                                  reduce_join_txt='; ',
+                                  skip_attr_all=True,
+                                  atk=None,
+                                  rcv=None):
+        types = getattr(ls, 'types', [])
+        attributes = getattr(ls, 'attributes', [])
+        hp_mult = getattr(ls, 'hp', 1)
+        # TODO: maybe we can just move min_atk and min_rcv in here
+        # TODO: had to add all these getattr because this is being used in the active
+        #       skill parser as well, is this right?
+        atk_mult = atk or getattr(ls, 'atk', 1)
+        rcv_mult = rcv or getattr(ls, 'rcv', 1)
+        damage_reduct = getattr(ls, 'shield', 0)
+        reduct_att = getattr(ls, 'reduction_attributes', [])
+        skill_text = ''
+
+        multiplier_text = self.fmt_multiplier_text(hp_mult, atk_mult, rcv_mult)
+        if multiplier_text:
+            skill_text += multiplier_text
+
+            for_skill_text = ''
+            if types:
+                for_skill_text += ' {} type'.format(self.types_format(types))
+
+            is_attr_all = len(attributes) in [0, 5]
+            should_skip_attr = is_attr_all and skip_attr_all
+
+            if attributes and not should_skip_attr:
+                if for_skill_text:
+                    for_skill_text += ' and'
+                color_text = 'all' if len(attributes) == 5 else self.attributes_format(attributes)
+                for_skill_text += ' ' + color_text + ' Att.'
+
+            if for_skill_text:
+                skill_text += ' for' + for_skill_text
+
+        reduct_text = self.fmt_reduct_text(damage_reduct, reduct_att)
+        if reduct_text:
+            if multiplier_text:
+                skill_text += reduce_join_txt
+            if not skill_text or ';' in reduce_join_txt:
+                reduct_text = reduct_text.capitalize()
+            skill_text += reduct_text
+
+        return skill_text
+
+    def fmt_multi_attr(self, attributes, conj='or'):
+        prefix = ''
+        if 1 <= len(attributes) <= 7:
+            attr_list = [self.ATTRIBUTES[i] for i in attributes]
+        elif 7 <= len(attributes) < 10:
+            # TODO: this is kind of weird maybe needs fixing
+            # All the attributes except the duplicate 'None' for fire, random, locked bomb, etc
+            non_attrs = [x for x in self.ATTRIBUTES.keys() if x is not None and x >= 0]
+            attrs = list(set(non_attrs) - set(attributes))
+            att_sym_diff = sorted(attrs, key=lambda x: self.ATTRIBUTES[x])
+            attr_list = [self.ATTRIBUTES[i] for i in att_sym_diff]
+            prefix = 'non '
+        else:
+            return '' if conj == 'or' else ' all'
+
+        return prefix + self.concat_list_and(attr_list, conj)
+
+    def fmt_multiplier_text(self, hp_mult, atk_mult, rcv_mult):
+        if hp_mult == atk_mult and atk_mult == rcv_mult:
+            if hp_mult == 1:
+                return None
+            return self.all_stats(fmt_mult(hp_mult))
+
+        mults = [(self.hp(), hp_mult), (self.atk(), atk_mult), (self.rcv(), rcv_mult)]
+        mults = list(filter(lambda x: x[1] != 1, mults))
+        mults.sort(key=lambda x: x[1], reverse=True)
+
+        chunks = []
+        x = 0
+        while x < len(mults):
+            can_check_double = x + 1 < len(mults)
+            if can_check_double and mults[x][1] == mults[x + 1][1]:
+                chunks.append(('{} & {}'.format(mults[x][0], mults[x + 1][0]), mults[x][1]))
+                x += 2
+            else:
+                chunks.append((mults[x][0], mults[x][1]))
+                x += 1
+
+        output = ''
+        for c in chunks:
+            if len(output):
+                output += ' and '
+            output += '{}x {}'.format(fmt_mult(c[1]), c[0])
+
+        return output
+
+    def fmt_reduct_text(self, shield, reduct_att=None):
+        if shield == 0:
+            return None
+        shield_text = fmt_mult(shield * 100)
+        if reduct_att in [None, [], [0, 1, 2, 3, 4]]:
+            return self.reduce_all_pct(shield_text)
+        else:
+            color_text = self.attributes_format(reduct_att)
+            return self.reduce_attr_pct(color_text, shield_text)
+
