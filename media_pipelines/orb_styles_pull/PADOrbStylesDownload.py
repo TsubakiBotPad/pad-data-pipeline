b3ae899a54fd1040167a17ced7eebc3f68ac98ff
nachoapps <nachoappsinc@gmail.com>
2020-01-25 16:29:21 -0500

    Fix pad orb styles

--- media_pipelines/orb_styles_pull/PADOrbStylesDownload.py
+++ media_pipelines/orb_styles_pull/PADOrbStylesDownload.py
@@ -12,26 +12,24 @@ parser = argparse.ArgumentParser(
 
 inputGroup = parser.add_argument_group("Input")
 inputGroup.add_argument("--server", required=True, help="na or jp")
-inputGroup.add_argument("--data_dir", required=True, help="Path to processed pad data files")
 
 outputGroup = parser.add_argument_group("Output")
-outputGroup.add_argument("--output_dir", help="Path to a folder where output should be saved")
-outputGroup.add_argument("--final_dir", help="Path to a folder where output should be saved")
+inputGroup.add_argument("--cache_dir", required=True, help="Path to a folder where output should be saved")
+outputGroup.add_argument("--output_dir", required=True, help="Path to a folder where output should be saved")
 
 helpGroup = parser.add_argument_group("Help")
 helpGroup.add_argument("-h", "--help", action="help", help="Displays this help message and exits.")
 args = parser.parse_args()
 
 server = args.server.lower()
+cache_dir = args.cache_dir
+output_dir = args.output_dir
 
 extras = []
 if args.server == 'na':
     extras = padtools.regions.north_america.server.extras
 elif args.server == 'jp':
     extras = padtools.regions.japan.server.extras
-
-output_dir = args.output_dir
-final_dir = args.final_dir
 
 
 def download_file(url, file_path):
@@ -44,18 +42,13 @@ def download_file(url, file_path):
 
 print('Found', len(extras), 'extras total')
 
-raw_dir = os.path.join(output_dir, 'raw')
-extract_dir = os.path.join(output_dir, 'extract')
-os.makedirs(raw_dir, exist_ok=True)
-os.makedirs(extract_dir, exist_ok=True)
-os.makedirs(final_dir, exist_ok=True)
-
-raw_dir = os.path.join(raw_dir, server)
-extract_dir = os.path.join(extract_dir, server)
-final_dir = os.path.join(final_dir, server)
-os.makedirs(raw_dir, exist_ok=True)
-os.makedirs(extract_dir, exist_ok=True)
-os.makedirs(final_dir, exist_ok=True)
+cache_dir = os.path.join(cache_dir, server)
+cache_raw_dir = os.path.join(cache_dir, 'raw')
+cache_extract_dir = os.path.join(cache_dir, 'extract')
+output_dir = os.path.join(output_dir, server)
+os.makedirs(cache_raw_dir, exist_ok=True)
+os.makedirs(cache_extract_dir, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
 
 python_exec = sys.executable
 cur_file_path = os.path.dirname(os.path.realpath(__file__))
@@ -66,10 +59,9 @@ should_always_process = False
 for extra in extras:
     raw_file_name = extra.file_name
     if not raw_file_name.startswith('block') or not raw_file_name.endswith('.btex'):
-        print('skipping', raw_file_name)
         continue
 
-    raw_file_path = os.path.join(raw_dir, raw_file_name)
+    raw_file_path = os.path.join(cache_raw_dir, raw_file_name)
     if os.path.exists(raw_file_path) and not should_always_process:
         print('file exists', raw_file_path)
     else:
@@ -77,20 +69,20 @@ for extra in extras:
         download_file(extra.url, raw_file_path)
 
     extract_file_name = raw_file_name.upper().replace('BTEX', 'PNG')
-    extract_file_path = os.path.join(extract_dir, extract_file_name)
+    extract_file_path = os.path.join(cache_extract_dir, extract_file_name)
 
     if os.path.exists(extract_file_path) and not should_always_process:
         print('skipping existing file', extract_file_path)
     else:
-        print('processing', raw_file_path, 'to', extract_dir, 'with name', extract_file_name)
+        print('processing', raw_file_path, 'to', cache_extract_dir, 'with name', extract_file_name)
         os.system('{python} {tool} -o={output} {input}'.format(
             python=python_exec,
             tool=tool_path,
             input=raw_file_path,
-            output=extract_dir))
+            output=output_dir))
 
     final_file_name = extract_file_name.replace('BLOCK', '').lower()
-    final_file_path = os.path.join(final_dir, final_file_name)
+    final_file_path = os.path.join(output_dir, final_file_name)
 
     if os.path.exists(final_file_path) and not should_always_process:
         print('skipping existing file', final_file_path)
