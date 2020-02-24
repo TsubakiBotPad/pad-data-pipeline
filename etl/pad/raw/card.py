aae0a7eeb3d613885d7323f19f32c2a3870b5f82
nachoapps <nachoappsinc@gmail.com>
2020-02-23 22:44:47 -0500

    Force flag values to boolean (to prevent issues when inserting)

--- etl/pad/raw/card.py
+++ etl/pad/raw/card.py
@@ -184,10 +184,10 @@ class Card(pad_util.Printable):
         self.latent_slot_unlock_flag = bool(self.random_flags & 32)
 
         # Composed with flags and other monster attributes
-        self.inheritable = self.inheritable_flag and self.active_skill_id
-        self.take_assists = self.take_assists_flag and self.active_skill_id
-        self.is_stackable = not self.unstackable_flag and self.type_1_id in [0, 12, 14]
-        self.usable = not self.assist_only_flag and self.monster_no < 100000
+        self.inheritable = bool(self.inheritable_flag and self.active_skill_id)
+        self.take_assists = bool(self.take_assists_flag and self.active_skill_id)
+        self.is_stackable = bool(not self.unstackable_flag and self.type_1_id in [0, 12, 14])
+        self.usable = bool(not self.assist_only_flag and self.monster_no < 100000)
 
         self.furigana = str(raw[67])  # JP data only?
         self.limit_mult = int(raw[68])
