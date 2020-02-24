95506257ba1b689b930aa80253624ba27d292315
nachoapps <nachoappsinc@gmail.com>
2020-02-23 21:46:29 -0500

    Bugfix; ES doesn't have raw description, as/ls do

--- utils/data_exporter.py
+++ utils/data_exporter.py
@@ -135,7 +135,6 @@ def dump_skill(f, css, converter, tag_extractor_fn):
     jp_skill = css.jp_skill
     na_skill = css.na_skill
     kr_skill = css.kr_skill
-    en_text = jp_skill.full_text(converter[1])
     skill_type_tags = tag_extractor_fn(css)
 
     f.write('# {}/{} - {}\n'.format(jp_skill.skill_id, jp_skill.skill_type, na_skill.name))
@@ -144,9 +143,9 @@ def dump_skill(f, css, converter, tag_extractor_fn):
         f.write('Stats: [{}, {}, {}, {}]\n'.format(jp_skill.hp, jp_skill.atk, jp_skill.rcv, jp_skill.shield))
 
     f.write('Game: {}\n'.format(na_skill.raw_description))
-    f.write('JP: {}\n'.format(jp_skill.full_text(converter[0])))
-    f.write('EN: {}\n'.format(en_text))
-    f.write('KR: {}\n'.format(jp_skill.full_text(converter[2])))
+    f.write('JP: {}\n'.format(jp_skill.full_text(converter[0]) or jp_skill.raw_description))
+    f.write('EN: {}\n'.format(jp_skill.full_text(converter[1]) or na_skill.raw_description))
+    f.write('KR: {}\n'.format(jp_skill.full_text(converter[2]) or kr_skill.raw_description))
     f.write('\n')
 
 
@@ -156,9 +155,9 @@ def dump_enemy_skill(f, css, converter):
     kr_skill = css.kr_skill
     f.write('# {}/{} - {}\n'.format(jp_skill.enemy_skill_id, jp_skill.type, na_skill.name))
     skill = BEHAVIOR_MAP[jp_skill.type](jp_skill)
-    f.write('JP: {}\n'.format(skill.description(converter[0]) or jp_skill.raw_description))
-    f.write('EN: {}\n'.format(skill.description(converter[1]) or na_skill.raw_description))
-    f.write('KR: {}\n'.format(skill.description(converter[2]) or kr_skill.raw_description))
+    f.write('JP: {}\n'.format(skill.description(converter[0])))
+    f.write('EN: {}\n'.format(skill.description(converter[1])))
+    f.write('KR: {}\n'.format(skill.description(converter[2])))
     f.write('\n')
 
 
