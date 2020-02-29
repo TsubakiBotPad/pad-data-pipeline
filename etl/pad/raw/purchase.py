7e0ca7e98d1051d81233b2526f78161dce5b39a8
nachoapps <nachoappsinc@gmail.com>
2020-02-29 11:56:35 -0500

    Use an override order for MonsterMp, bugfix for repeated insertions.
    Also autoformat and minor cleanups.

--- etl/pad/raw/purchase.py
+++ etl/pad/raw/purchase.py
@@ -12,16 +12,17 @@ from pad.common.shared_types import Server
 
 FILE_NAME = 'shop_item.json'
 
+
 class Purchase(Printable):
     """Buyable monsters."""
 
-    def __init__(self, raw: List[str], server: Server, tbegin, tend):
+    def __init__(self, raw: List[str], server: Server, tbegin: str, tend: str):
         self.server = server
         self.start_time_str = tbegin
         self.start_timestamp = pad_util.gh_to_timestamp_2(self.start_time_str, server)
         self.end_time_str = tend
         self.end_timestamp = pad_util.gh_to_timestamp_2(self.end_time_str, server)
-        self.type = str(raw[0])   # Should be P
+        self.type = str(raw[0])  # Should be P
 
         # Trade monster ID
         self.monster_id = int(raw[1])
@@ -42,13 +43,14 @@ class Purchase(Printable):
 def load_data(server: Server, data_dir: str = None, json_file: str = None) -> List[Purchase]:
     """Load Card objects from PAD JSON file."""
     data_json = pad_util.load_raw_json(data_dir, json_file, FILE_NAME)
-    tdata = None, None
+    start_time, end_time = None, None
     mpbuys = []
     for item in filter(None, data_json['d'].split('\n')):
         raw = item.split(',')
         if raw[0] == 'T':
-            tdata = raw[1], raw[2]
+            start_time = raw[1]
+            end_time = raw[2]
         else:
-            p = Purchase(raw, server, *tdata)
+            p = Purchase(raw, server, start_time, end_time)
             mpbuys.append(p)
-    return mpbuys # This will have a lot or repeats, but that shouldn't matter
+    return mpbuys  # This will have a lot or repeats, but that shouldn't matter
--- etl/pad/storage_processor/purchase_processor.py
+++ etl/pad/storage_processor/purchase_processor.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from typing import List
 
 from pad.db.db_util import DbWrapper
 from pad.common.shared_types import Server
@@ -8,6 +9,7 @@ from pad.storage.monster import MonsterWithMPValue
 from pad.raw_processor import crossed_data
 
 logger = logging.getLogger('processor')
+
 
 class PurchaseProcessor(object):
     def __init__(self, data: crossed_data.CrossServerDatabase):
@@ -23,6 +25,15 @@ class PurchaseProcessor(object):
             for raw in purchase_map:
                 logger.debug('Creating purchase: %s', raw)
                 p_item = Purchase.from_raw_purchase(raw)
-                m_item = MonsterWithMPValue(monster_id=pdata.target_monster_id, buy_mp=pdata.mp_cost)
                 db.insert_or_update(p_item)
-                db.insert_or_update(m_item)
+
+        def purchases_to_map(purchases: List[Purchase]):
+            return {x.monster_id: x.cost for x in purchases}
+
+        monster_id_to_mp = purchases_to_map(self.purchase_data[Server.kr])
+        monster_id_to_mp.update(purchases_to_map(self.purchase_data[Server.na]))
+        monster_id_to_mp.update(purchases_to_map(self.purchase_data[Server.jp]))
+
+        for monster_id, mp_cost in monster_id_to_mp.items():
+            m_item = MonsterWithMPValue(monster_id=monster_id, buy_mp=mp_cost)
+            db.insert_or_update(m_item)
