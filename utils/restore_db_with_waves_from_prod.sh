89da0aecb0c2bf54c8ffdf7ad05efafbf304a7fa
nachoapps <nachoappsinc@gmail.com>
2020-04-20 19:11:10 -0400

    add scripts to restore with wave data

---
+++ utils/restore_db_with_waves_from_prod.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+user=$(grep user ../etl/db_config.json | sed -r 's/.*: "(.*)",/\1/')
+pword=$(grep password ../etl/db_config.json | sed -r 's/.*: "(.*)",/\1/')
+input_file="../pad_data/db/dadguide_wave_data.mysql"
+
+echo "using credentials: ${user} ${pword}"
+mysql -u ${user} -p${pword} dadguide <${input_file}
