b6c559e9143bc87f75d915bc85b14238c2ebe23e
AA <chasehult@gmail.com>
2020-04-24 17:27:28 -0700

    Bonus combo jp (#79)
    * JP Color Combo Bonus Combo Text
    
    * Fixed Previous Commit

--- etl/pad/raw/skills/jp/leader_skill_text.py
+++ etl/pad/raw/skills/jp/leader_skill_text.py
@@ -340,6 +340,15 @@ class JpLSTextConverter(JpBaseTextConverter):
         from ..en.leader_skill_text import EnLSTextConverter
         return EnLSTextConverter().color_combo_bonus_combo_text(ls)
 
+    def color_combo_bonus_combo_text(self, ls):
+        if len(ls.attributes) and ls.attributes[1:] != ls.attributes[:-1]:
+            cond = '{}の同時攻擊'.format(self.fmt_multi_attr(list(set(ls.attributes))))
+        elif not ls.attributes:
+            cond += '{}の{}コンボ以上'.format(self.fmt_multi_attr(list(set(ls.attributes))), ls.min_combo)
+        else:
+            cond += '{}コンボ以上'.format(ls.min_combo)
+        return cond + "で{}コンボ加算".format(ls.bonus_combos)
+
     def full_text(self, text, tags=None):
         tags = tags or []
         if isinstance(text, (str, type(None))):
