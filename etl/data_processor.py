4b2c84f5771e0505898a9c45dea5721df0e17c5e
nachoapps <nachoappsinc@gmail.com>
2020-05-18 20:08:59 +0000

    enable jp for padspike as well

--- etl/data_processor.py
+++ etl/data_processor.py
@@ -125,8 +125,8 @@ def load_data(args):
 
     if not args.skipintermediate:
         logger.info('Storing intermediate data')
-        # jp_database.save_all(args.output_dir, args.pretty)
         # This is supported for PadSpike, until we can support it better in the dg db
+        jp_database.save_all(args.output_dir, args.pretty)
         na_database.save_all(args.output_dir, args.pretty)
         # kr_database.save_all(args.output_dir, args.pretty)
 
