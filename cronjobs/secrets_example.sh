c2c4058142a26bf53da687d009ecd0e88b46d7d0
TsubakiBot <TsubakiBotPAD@gmail.com>
2020-08-25 00:48:26 +0000

    Clean up more + add req

---
+++ cronjobs/secrets_example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -x
+
+declare -rx AWS_ACCESS_KEY_ID="GetThisFromAWSCred"
+declare -rx AWS_SECRET_ACCESS_KEY="FromAWsConsole"
+declare -rx MYSQL_USER="whateverYouMadeYourSqlUserNameToBe"
+declare -rx MYSQL_PASSWORD="your sql password"
