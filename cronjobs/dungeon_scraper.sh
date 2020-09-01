3093965be8012b067c3ef0c0d42ed4392cf3878d
TsubakiBot <TsubakiBotPAD@gmail.com>
2020-09-01 08:15:35 +0000

    removing hardcoded call to pad_dg_pull

--- cronjobs/dungeon_scraper.sh
+++ cronjobs/dungeon_scraper.sh
@@ -10,16 +10,16 @@ flock -xn /tmp/dg_scraper_na.lck python3 ${REPO_ROOT}/etl/auto_dungeon_scrape.py
   --input_dir=${RAW_DIR} \
   --doupdates \
   --server=na \
-  --group=red \
-  --user_uuid="00000000-0000-0000-00000000000000000" \
-  --user_intid="111111111"
+  --group=${NA_PAD_USER_COLOR_GROUP} \
+  --user_uuid=${NA_PAD_USER_UUID} \
+  --user_intid=${NA_PAD_USER_INTID}
 
 flock -xn /tmp/dg_scraper_jp.lck python3 ${REPO_ROOT}/etl/auto_dungeon_scrape.py \
   --db_config=${DB_CONFIG} \
   --input_dir=${RAW_DIR} \
   --doupdates \
   --server=jp \
-  --group=blue \
-  --user_uuid="00000000-0000-0000-00000000000000000" \
-  --user_intid="111111111"
+  --group=${JP_PAD_USER_COLOR_GROUP} \
+  --user_uuid=${JP_PAD_USER_UUID} \
+  --user_intid=${JP_PAD_USER_INTID}
 
