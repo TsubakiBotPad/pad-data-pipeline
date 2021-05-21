d45d5b97123fe1a00c577db485687011d97de24a
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-05-20 22:01:05 -0500

    Revert "Don't override files on exports (#147)"
    This reverts commit 6b138e6c5d9584bf056d0f2db708d50a87b48e77.

--- cronjobs/export_data.sh
+++ cronjobs/export_data.sh
@@ -7,46 +7,7 @@ source ./discord.sh
 source ./shared.sh
 
 echo "Exporting DG (NO WAVES) to sqlite"
-case ${1^^} in
-  '')
-    DADGUIDE_DB_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide.sqlite";
-    DADGUIDE_ZIP_DB_FILE="${DADGUIDE_GAME_DB_DIR}/v2_dadguide.sqlite.zip";
-    DADGUIDE_MYSQL_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide.mysql";
-    DADGUIDE_MYSQL_ZIP_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide.mysql.zip";
-    DADGUIDE_MYSQL_WAVE_DATA_FILE="/tmp/dadguide_wave_data.mysql"
-    DADGUIDE_MYSQL_WAVE_DATA_ZIP_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_wave_data.mysql.zip";
-    ;;
-  JP)
-    DADGUIDE_DB_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_jp.sqlite";
-    DADGUIDE_ZIP_DB_FILE="${DADGUIDE_GAME_DB_DIR}/v2_dadguide_jp.sqlite.zip";
-    DADGUIDE_MYSQL_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_jp.mysql";
-    DADGUIDE_MYSQL_ZIP_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_jp.mysql.zip";
-    DADGUIDE_MYSQL_WAVE_DATA_FILE="/tmp/dadguide_wave_data_jp.mysql"
-    DADGUIDE_MYSQL_WAVE_DATA_ZIP_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_wave_data_jp.mysql.zip";
-    ;;
-  NA)
-    DADGUIDE_DB_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_na.sqlite";
-    DADGUIDE_ZIP_DB_FILE="${DADGUIDE_GAME_DB_DIR}/v2_dadguide_na.sqlite.zip";
-    DADGUIDE_MYSQL_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_na.mysql";
-    DADGUIDE_MYSQL_ZIP_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_na.mysql.zip";
-    DADGUIDE_MYSQL_WAVE_DATA_FILE="/tmp/dadguide_wave_data_na.mysql"
-    DADGUIDE_MYSQL_WAVE_DATA_ZIP_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_wave_data_na.mysql.zip";
-    ;;
-  KR)
-    DADGUIDE_DB_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_kr.sqlite";
-    DADGUIDE_ZIP_DB_FILE="${DADGUIDE_GAME_DB_DIR}/v2_dadguide_kr.sqlite.zip";
-    DADGUIDE_MYSQL_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_kr.mysql";
-    DADGUIDE_MYSQL_ZIP_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_kr.mysql.zip";
-    DADGUIDE_MYSQL_WAVE_DATA_FILE="/tmp/dadguide_wave_data_kr.mysql"
-    DADGUIDE_MYSQL_WAVE_DATA_ZIP_FILE="${DADGUIDE_GAME_DB_DIR}/dadguide_wave_data_kr.mysql.zip";
-    ;;
-
-  *)
-    echo "The first positional argument must be NA, JP, or KR."
-    exit 1
-  ;;
-esac
-
+DADGUIDE_DB_FILE=${DADGUIDE_GAME_DB_DIR}/dadguide.sqlite
 DADGUIDE_DB_FILE_TMP=${DADGUIDE_DB_FILE}_tmp
 rm -f "${DADGUIDE_DB_FILE_TMP}"
 ${SCHEMA_TOOLS_DIR}/mysql2sqlite.sh \
@@ -59,12 +20,15 @@ ${SCHEMA_TOOLS_DIR}/mysql2sqlite.sh \
 mv "${DADGUIDE_DB_FILE_TMP}" "${DADGUIDE_DB_FILE}"
 
 echo "Zipping/copying DB dump"
+DADGUIDE_ZIP_DB_FILE="${DADGUIDE_GAME_DB_DIR}/v2_dadguide.sqlite.zip"
 DADGUIDE_ZIP_DB_FILE_TMP="${DADGUIDE_ZIP_DB_FILE}_tmp"
 rm -f "${DADGUIDE_ZIP_DB_FILE_TMP}"
 zip -j "${DADGUIDE_ZIP_DB_FILE_TMP}" "${DADGUIDE_DB_FILE}"
 mv "${DADGUIDE_ZIP_DB_FILE_TMP}" "${DADGUIDE_ZIP_DB_FILE}"
 
 echo "Exporting DG (NO WAVES) to mysql"
+DADGUIDE_MYSQL_FILE=${DADGUIDE_GAME_DB_DIR}/dadguide.mysql
+DADGUIDE_MYSQL_ZIP_FILE=${DADGUIDE_GAME_DB_DIR}/dadguide.mysql.zip
 rm -f "${DADGUIDE_MYSQL_FILE}" "${DADGUIDE_MYSQL_ZIP_FILE}"
 mysqldump --default-character-set=utf8 \
   -u ${MYSQL_USER} \
@@ -75,6 +39,8 @@ mysqldump --default-character-set=utf8 \
 zip -j "${DADGUIDE_MYSQL_ZIP_FILE}" "${DADGUIDE_MYSQL_FILE}"
 
 echo "Exporting zipped full DG+WAVES to mysql"
+DADGUIDE_MYSQL_WAVE_DATA_FILE=/tmp/dadguide_wave_data.mysql
+DADGUIDE_MYSQL_WAVE_DATA_ZIP_FILE=${DADGUIDE_GAME_DB_DIR}/dadguide_wave_data.mysql.zip
 rm -f "${DADGUIDE_MYSQL_WAVE_DATA_FILE}" "${DADGUIDE_MYSQL_WAVE_DATA_ZIP_FILE}"
 mysqldump --default-character-set=utf8 \
   -u ${MYSQL_USER} \
@@ -85,6 +51,8 @@ zip -j "${DADGUIDE_MYSQL_WAVE_DATA_ZIP_FILE}" "${DADGUIDE_MYSQL_WAVE_DATA_FILE}"
 rm -f "${DADGUIDE_MYSQL_WAVE_DATA_FILE}"
 
 echo "Exporting zipped full DG+WAVES to sqlite"
+DADGUIDE_WAVE_DATA_FILE=/tmp/dadguide_wave_data.sqlite
+DADGUIDE_WAVE_DATA_ZIP_FILE=${DADGUIDE_GAME_DB_DIR}/dadguide_wave_data.sqlite.zip
 rm -f "${DADGUIDE_WAVE_DATA_FILE}" "${DADGUIDE_WAVE_DATA_ZIP_FILE}"
 ${SCHEMA_TOOLS_DIR}/mysql2sqlite.sh \
   -u ${MYSQL_USER} \
