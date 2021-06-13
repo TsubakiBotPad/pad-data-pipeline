92d59b477d32505dd6264db13f522709fc42eece
AA <chasehult@gmail.com>
2021-06-13 02:05:26 -0700

    Prevent dupes in intersection tables (#183)

--- etl/pad/storage/egg_machines_monsters.py
+++ etl/pad/storage/egg_machines_monsters.py
@@ -1,4 +1,5 @@
-from pad.db.sql_item import SimpleSqlItem
+from pad.db import sql_item
+from pad.db.sql_item import SimpleSqlItem, ExistsStrategy
 
 
 class EggMachinesMonster(SimpleSqlItem):
@@ -15,6 +16,22 @@ class EggMachinesMonster(SimpleSqlItem):
         self.monster_id = monster_id
         self.roll_chance = roll_chance
         self.egg_machine_id = egg_machine_id
+
+    def exists_strategy(self):
+        return ExistsStrategy.BY_VALUE
+
+    def value_exists_sql(self):
+        sql = """
+        SELECT egg_machine_monster_id FROM {table}
+        WHERE monster_id = {monster_id} AND egg_machine_id = {egg_machine_id}
+        """.format(table=self._table(), **sql_item.object_to_sql_params(self))
+        return sql
+
+    def _non_auto_insert_cols(self):
+        return [self._key()]
+
+    def _non_auto_update_cols(self):
+        return [self._key()]
 
     def __str__(self):
         return 'EggMachineMonster ({}-{}-{}-{})'.format(self.egg_machine_monster_id, self.monster_id, self.roll_chance,
