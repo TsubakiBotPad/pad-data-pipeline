ddc516b7eb11936bbc3cf483f5746b14fb1dbe23
AA <chasehult@gmail.com>
2021-08-04 03:04:40 -0700

    Change AwakeningProcessor to AwokenSkillProcessor
    Closes #156
    Idk why that's been open for so long

--- etl/pad/storage_processor/awoken_skill_processor.py
+++ etl/pad/storage_processor/awoken_skill_processor.py
@@ -7,7 +7,7 @@ from pad.storage.awoken_skill import AwokenSkill
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
-class AwakeningProcessor(object):
+class AwokenSkillProcessor(object):
     def __init__(self):
         with open(os.path.join(__location__, 'awoken_skill.json')) as f:
             self.awoken_skills = json.load(f)
