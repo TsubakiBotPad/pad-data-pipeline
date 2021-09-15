87707fe55881a67ac57785de73a67fbc218d3bf5
AA <chasehult@gmail.com>
2021-09-15 15:19:25 -0700

    Whoops

--- cronjobs/shared_roots_example.sh
+++ cronjobs/shared_roots_example.sh
@@ -5,4 +5,4 @@ set -x
 
 declare -rx REPO_ROOT="/home/bot/pad-data-pipeline"
 declare -rx PAD_RESOURCES_ROOT="/home/bot/pad-resources"
-declare -rx ES_DIR="/home/bot/pad-game-data-slim"
+declare -rx GAME_DATA_DIR="/home/bot/pad-game-data-slim"
