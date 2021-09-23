bc67f644638164654d5e226047738c020bc96663
AA <chasehult@gmail.com>
2021-09-23 01:00:30 -0700

    Create export_game_data.sh

---
+++ cronjobs/game_data/export_game_data.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")" || exit
+source ../shared_root.sh
+source ../shared.sh
+source ../discord.sh
+
+echo "Exporting Enemy Skills"
+./export_enemy_skills.sh
+
+echo "Exporting PAD Assets"
+./export_pad_assets.sh
+
+echo "Exporting PAD Data"
+./export_pad_data.sh
+
+hook_info "Game data export finished"
