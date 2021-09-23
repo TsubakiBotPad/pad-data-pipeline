79a4f552df5de9808c951ca80cd3f5afd820be9f
AA <chasehult@gmail.com>
2021-09-23 08:41:40 -0700

    Remove asset export

--- cronjobs/game_data/export_pad_data.sh
+++ cronjobs/game_data/export_pad_data.sh
@@ -5,8 +5,17 @@ source ../shared.sh
 source "${VENV_ROOT}/bin/activate"
 
 cd "${GAME_DATA_DIR}" || exit
+git add behavior*
+git commit -m 'manually approved ES' || true
+
 git pull --rebase --autostash
 
 python3 "${UTILS_ETL_DIR}/data_exporter.py" \
   --input_dir="${RAW_DIR}" \
   --output_dir="${GAME_DATA_DIR}"
+
+git add ./*/assets/
+git add ./*/extras/
+git add *.txt
+git commit -m 'Data update' || true
+git push
