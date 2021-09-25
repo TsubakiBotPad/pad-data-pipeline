621cc5e56c885efd8e5f4d57011fb5455ae1111d
AA <chasehult@gmail.com>
2021-09-24 19:25:46 -0700

    Fix Autodungeon (#264)

--- cronjobs/dungeon_scraper.sh
+++ cronjobs/dungeon_scraper.sh
@@ -6,7 +6,33 @@ set -x
 cd "$(dirname "$0")" || exit
 source ./shared_root.sh
 source ./shared.sh
+source ./discord.sh
 source "${VENV_ROOT}/bin/activate"
+
+function error_exit() {
+  hook_error "Autodungeon failed <@&${NOTIFICATION_DISCORD_ROLE_ID}>"
+  hook_file "/tmp/dg_scraper_log.txt"
+}
+
+function success_exit() {
+  echo "Autodungeon finished"
+}
+
+function human_fixes_check() {
+  processor_fails="/tmp/autodungeon_processor_issues.txt"
+  if [[ -s ${processor_fails} ]]; then
+    echo "Alerting for autodungeon processor issues"
+    hook_warn "\`\`\`\n$(cat "${processor_fails}" \
+                       | sed ':a;N;$!ba;s/\n/\\n/g' \
+                       | head -c 1990)\n\`\`\`"
+  else
+    echo "Autodungeon completely successful"
+  fi
+}
+
+# Enable alerting to discord
+trap error_exit ERR
+trap success_exit EXIT
 
 flock -xn /tmp/dg_scraper_na.lck python3 ${REPO_ROOT}/etl/auto_dungeon_scrape.py \
   --db_config=${DB_CONFIG} \
@@ -17,6 +43,8 @@ flock -xn /tmp/dg_scraper_na.lck python3 ${REPO_ROOT}/etl/auto_dungeon_scrape.py
   --user_uuid=${NA_PAD_USER_UUID} \
   --user_intid=${NA_PAD_USER_INTID}
 
+human_fixes_check
+
 flock -xn /tmp/dg_scraper_jp.lck python3 ${REPO_ROOT}/etl/auto_dungeon_scrape.py \
   --db_config=${DB_CONFIG} \
   --input_dir=${RAW_DIR} \
@@ -25,3 +53,7 @@ flock -xn /tmp/dg_scraper_jp.lck python3 ${REPO_ROOT}/etl/auto_dungeon_scrape.py
   --group=${JP_PAD_USER_COLOR_GROUP} \
   --user_uuid=${JP_PAD_USER_UUID} \
   --user_intid=${JP_PAD_USER_INTID}
+
+human_fixes_check
+
+hook_info "Autodungeon finished"
