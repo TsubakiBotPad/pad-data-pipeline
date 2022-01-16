e88483db2d3f913988b6790b9a78769a3e02db1d
AA <chasehult@gmail.com>
2022-01-16 11:33:52 -0700

    Stop purging Schedule data (#321)

--- etl/pad/storage_processor/purge_data_processor.py
+++ etl/pad/storage_processor/purge_data_processor.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 
 from pad.db.db_util import DbWrapper
 
@@ -10,24 +10,22 @@ def date2tstamp(date):
     return int(date.timestamp())
 
 
-class PurgeDataProcessor(object):
-    def __init__(self):
-        pass
-
+class PurgeDataProcessor:
     def process(self, db: DbWrapper):
-        print('Starting deletion of old records')
-        print('schedule size:', db.get_single_value('select count(*) from schedule'))
-        print('deleted_rows size', db.get_single_value('select count(*) from deleted_rows'))
-
-        # This is a hint to mysql that we shouldn't insert into deleted_rows
-        # while purging. The client should handle deleting old events in bulk.
-        db.fetch_data('set @TRIGGER_DISABLED=true')
-
-        delete_timestamp = date2tstamp(datetime.now() - timedelta(weeks=4))
-        print('deleting before', delete_timestamp)
-        schedule_deletes = db.update_item(
-            "DELETE FROM `schedule` WHERE end_timestamp < {}".format(delete_timestamp))
-        deleted_row_deletes = db.update_item(
-            "DELETE FROM `deleted_rows` WHERE tstamp < {}".format(delete_timestamp))
-
-        logger.info("purged {} old schedules and {} old deleted_rows".format(schedule_deletes, deleted_row_deletes))
+        pass
+        # print('Starting deletion of old records')
+        # print('schedule size:', db.get_single_value('select count(*) from schedule'))
+        # print('deleted_rows size', db.get_single_value('select count(*) from deleted_rows'))
+        #
+        # # This is a hint to mysql that we shouldn't insert into deleted_rows
+        # # while purging. The client should handle deleting old events in bulk.
+        # db.fetch_data('set @TRIGGER_DISABLED=true')
+        #
+        # delete_timestamp = date2tstamp(datetime.now() - timedelta(weeks=4))
+        # print('deleting before', delete_timestamp)
+        # schedule_deletes = db.update_item(
+        #     "DELETE FROM `schedule` WHERE end_timestamp < {}".format(delete_timestamp))
+        # deleted_row_deletes = db.update_item(
+        #     "DELETE FROM `deleted_rows` WHERE tstamp < {}".format(delete_timestamp))
+        #
+        # logger.info("purged {} old schedules and {} old deleted_rows".format(schedule_deletes, deleted_row_deletes))
