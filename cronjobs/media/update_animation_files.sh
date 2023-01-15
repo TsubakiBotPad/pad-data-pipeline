2a7202c7b461bdd935e5df6a2ee1c5dccc0e93bd
AA <chasehult@gmail.com>
2023-01-14 22:14:16 -0700

    Remove logging

--- cronjobs/media/update_animation_files.sh
+++ cronjobs/media/update_animation_files.sh
@@ -5,21 +5,6 @@
 cd "$(dirname "$0")" || exit
 source ../shared_root.sh
 source ../shared.sh
-source ../discord.sh
-
-function error_exit() {
-  hook_error "Image Pipeline failed <@&${NOTIFICATION_DISCORD_ROLE_ID}>"
-  hook_file "/tmp/dg_image_log.txt"
-}
-
-function success_exit() {
-  echo "Image pipeline finished"
-  hook_info "Image pipeline finished"
-}
-
-# Enable alerting to discord
-trap error_exit ERR
-trap success_exit EXIT
 
 set +x
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
