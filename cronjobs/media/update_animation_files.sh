124da8802689d41199f824bb9362fcece3bc4d82
AA <chasehult@gmail.com>
2023-01-19 12:36:49 -0700

    Fix dirs

--- cronjobs/media/update_animation_files.sh
+++ cronjobs/media/update_animation_files.sh
@@ -19,7 +19,8 @@ yarn --cwd=${PAD_RESOURCES_ROOT} extract "${PAD_RESOURCES_ROOT}/data/bin" "${IMG
   --animated-only --for-tsubaki --new-only
 xvfb-run -s "-ac -screen 0 640x388x24" \
   yarn --cwd=${PAD_RESOURCES_ROOT} render "${IMG_DIR}/spine" \
-  --animated-dir "${IMG_DIR}/animated" --still-dir "${IMG_DIR}/jp/portrait/corrected_data" \
+  --animated-dir "${PAD_RESOURCES_ROOT}/animated" \
+  --still-dir "${IMG_DIR}/jp/portrait/corrected_data" \
   --new-only --for-tsubaki "s3://tsubakibotpad/media/animated_portraits/"
 
 # Force a sync
