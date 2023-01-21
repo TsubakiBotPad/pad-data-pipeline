802f44a25ed4cd405ff32f448f1bd35f274716d3
AA <chasehult@gmail.com>
2023-01-20 21:19:17 -0700

    Add tombstones

--- cronjobs/media/update_animation_files.sh
+++ cronjobs/media/update_animation_files.sh
@@ -13,13 +13,13 @@ nvm use 16
 set -x
 
 # Animations
-yarn --cwd=${PAD_RESOURCES_ROOT} update "${PAD_RESOURCES_ROOT}/data/bin" \
+yarn --cwd=${PAD_RESOURCES_ROOT} update "${IMG_DIR}/jp/portrait/raw_data" \
   --for-tsubaki --new-only
-yarn --cwd=${PAD_RESOURCES_ROOT} extract "${PAD_RESOURCES_ROOT}/data/bin" "${IMG_DIR}/spine" \
+yarn --cwd=${PAD_RESOURCES_ROOT} extract "${IMG_DIR}/jp/portrait/raw_data" "${IMG_DIR}/spine" \
   --animated-only --for-tsubaki --new-only
 xvfb-run -s "-ac -screen 0 640x388x24" \
   yarn --cwd=${PAD_RESOURCES_ROOT} render "${IMG_DIR}/spine" \
-  --animated-dir "${PAD_RESOURCES_ROOT}/data/animated" \
+  --animated-dir "${IMG_DIR}/animated" \
   --still-dir "${IMG_DIR}/jp/portrait/corrected_data" \
   --new-only --for-tsubaki "s3://tsubakibotpad/media/animated_portraits/"
 
