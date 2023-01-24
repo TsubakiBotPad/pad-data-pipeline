fb79ebf9a0efdd1757392a40b0a881de64a6a473
AA <chasehult@gmail.com>
2023-01-24 00:43:18 -0700

    Use config.json

--- cronjobs/media/update_animation_files.sh
+++ cronjobs/media/update_animation_files.sh
@@ -21,7 +21,7 @@ xvfb-run -s "-ac -screen 0 640x388x24" \
   yarn --cwd=${PAD_RESOURCES_ROOT} render "${IMG_DIR}/spine" \
   --animated-dir "${IMG_DIR}/animated" \
   --still-dir "${IMG_DIR}/jp/portrait/corrected_data" \
-  --new-only --for-tsubaki "s3://tsubakibotpad/media/animated_portraits/"
+  --new-only --for-tsubaki
 
 # Force a sync
 ${CRONJOBS_DIR}/sync_data.sh
