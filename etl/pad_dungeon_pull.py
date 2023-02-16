dd154b5bb1ec041760845f900782a0bd04643fa6
AA <chasehult@gmail.com>
2023-02-15 22:43:55 -0700

    Fix autodungeon

--- etl/pad_dungeon_pull.py
+++ etl/pad_dungeon_pull.py
@@ -1,13 +1,16 @@
 import argparse
 import json
 import logging
+import math
+import os
 import time
 
 from tqdm import tqdm
 
 from pad.api import pad_api
-
+from pad.common.shared_types import Server
 from pad.db.db_util import DbWrapper
+from pad.raw import bonus
 from pad.storage.wave import WaveItem
 
 
@@ -15,6 +18,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Extracts PAD dungeon data.", add_help=False)
 
     input_group = parser.add_argument_group("Input")
+    input_group.add_argument("--base_dir", required=True, help="Base directory for jsons")
     input_group.add_argument("--server", required=True, help="One of [NA, JP]")
     input_group.add_argument("--user_uuid", required=True, help="Account UUID")
     input_group.add_argument("--user_intid", required=True, help="Account code")
@@ -22,6 +26,8 @@ def parse_args():
     input_group.add_argument("--dungeon_id", required=True, help="Dungeon ID")
     input_group.add_argument("--floor_id", required=True, help="Floor ID")
     input_group.add_argument("--loop_count", type=int, default=100, help="Number of entry attempts")
+    input_group.add_argument("--stam_adjust", default=False,
+                             action="store_true", help="Try to enter with 0 stamina.")
 
     output_group = parser.add_argument_group("Output")
     output_group.add_argument("--db_config", required=True, help="JSON database info")
@@ -40,16 +46,16 @@ def pull_data(args, api_client=None, db_wrapper=None):
     if args.logsql:
         logging.getLogger('database').setLevel(logging.DEBUG)
 
-    server = args.server.upper()
+    if args.server.upper() == 'NA':
+        endpoint = pad_api.ServerEndpoint.NA
+        server = Server.na
+    elif args.server.upper() == 'JP':
+        endpoint = pad_api.ServerEndpoint.JA
+        server = Server.jp
+    else:
+        raise Exception('unexpected server:' + args.server)
 
     if api_client is None:
-        if server == 'NA':
-            endpoint = pad_api.ServerEndpoint.NA
-        elif server == 'JP':
-            endpoint = pad_api.ServerEndpoint.JA
-        else:
-            raise Exception('unexpected server:' + args.server)
-
         api_client = pad_api.PadApiClient(endpoint, args.user_uuid, args.user_intid)
         api_client.login()
         print('load_player_data')
@@ -71,14 +77,28 @@ def pull_data(args, api_client=None, db_wrapper=None):
 
     stamina = db_wrapper.get_single_value(f"SELECT stamina FROM sub_dungeons"
                                           f" WHERE sub_dungeon_id = {int(dungeon_id) * 1000 + int(floor_id)};")
+    bonuses = bonus.load_bonus_data(data_dir=os.path.join(args.base_dir, args.server.lower()), server=server)
+    now = time.time()
+    stam_bonus = next((b for b in bonuses
+                       if b.start_timestamp <= now <= b.end_timestamp
+                       and b.dungeon_id == dungeon_id
+                       and (b.sub_dungeon_id is None or b.sub_dungeon_id % 1000 == floor_id)
+                       and b.bonus_id == 5), None)
+    if stam_bonus is not None:
+        if stam_bonus.bonus_value:
+            stamina = math.ceil(stamina * stam_bonus.bonus_value / 10000)
+        elif stam_bonus.bonus_value_2:
+            stamina = stam_bonus.bonus_value_2
+        else:
+            stamina = 0
     entry_id = int(db_wrapper.get_single_value("SELECT MAX(entry_id) FROM wave_data;"))
-
+    if args.stam_adjust:
+        stamina = 0
+    print(f'entering {args.server.upper()} dungeon {dungeon_id} floor {floor_id} {loop_count} times using {stamina} stamina.')
     if args.stream_safe:
         iterator = range(loop_count)
     else:
         iterator = tqdm(range(loop_count), unit='runs')
-
-    print('entering', server, 'dungeon', dungeon_id, 'floor', floor_id, loop_count, 'times')
     for _ in iterator:
         entry_id += 1
         entry_json = api_client.enter_dungeon(dungeon_id, floor_id, self_card=friend_card, stamina=stamina)
@@ -87,9 +107,9 @@ def pull_data(args, api_client=None, db_wrapper=None):
 
         for stage_idx, floor in enumerate(wave_response.floors):
             for monster_idx, monster in enumerate(floor.monsters):
-                wave_item = WaveItem(pull_id=pull_id, entry_id=entry_id, server=server, dungeon_id=dungeon_id,
-                                     floor_id=floor_id, stage=stage_idx, slot=monster_idx, monster=monster,
-                                     leader_id=leaders[0], friend_id=leaders[1])
+                wave_item = WaveItem(pull_id=pull_id, entry_id=entry_id, server=args.server.upper(),
+                                     dungeon_id=dungeon_id, floor_id=floor_id, stage=stage_idx, slot=monster_idx,
+                                     monster=monster, leader_id=leaders[0], friend_id=leaders[1])
                 db_wrapper.insert_item(wave_item.insert_sql())
 
         if server != 'NA':
