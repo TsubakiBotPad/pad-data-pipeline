5b666a06816a287da0e4b12e62a7b21eeaa20dd1
nurungji0715 <112784852+nurungji0715@users.noreply.github.com>
2023-04-02 10:28:59 +0900

    Update PADHQImageDownload.py (#469)
    I used async code to do multiple https requests at the same time, so the code takes way faster to run.

--- media_pipelines/assets/PADHQImageDownload.py
+++ media_pipelines/assets/PADHQImageDownload.py
@@ -1,10 +1,11 @@
 import argparse
+import asyncio
 import json
 import os
 import re
+import aiohttp
 
 import pymysql
-import requests
 
 parser = argparse.ArgumentParser(
     description="Downloads P&D images from the GungHo site.", add_help=False)
@@ -18,6 +19,7 @@ outputGroup.add_argument("--output_dir", help="Path to a folder where output sho
 
 helpGroup = parser.add_argument_group("Help")
 helpGroup.add_argument("-h", "--help", action="help", help="Displays this help message and exits.")
+
 args = parser.parse_args()
 raw_dir = args.raw_file_dir
 corrected_dir = args.output_dir
@@ -25,51 +27,63 @@ corrected_dir = args.output_dir
 GUNGHO_TEMPLATE = 'https://pad.gungho.jp/member/img/graphic/illust/{}'
 
 
-def download_file(url, file_path):
-    r = requests.get(url, allow_redirects=False)
-    if r.status_code == 302:
-        raise Exception("No HQ Image")
-    if r.status_code != 200:
-        raise Exception('Bad status code:', r.status_code)
+async def download_file(url, file_path, monster_id, cursor):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, allow_redirects=False) as response:
+            if response.status == 302:
+                return
+            if response.status != 200:
+                return
+            image_data = await response.content.read()
 
     with open(file_path, "wb") as f:
-        f.write(r.content)
+        f.write(image_data)
 
-    return len(r.content)
+    cursor.execute('''INSERT INTO monster_image_sizes (monster_id, hq_png_size, tstamp) 
+                               VALUES (%s, %s, UNIX_TIMESTAMP()) 
+                               ON DUPLICATE KEY 
+                               UPDATE hq_png_size=%s, tstamp=UNIX_TIMESTAMP();''',
+                   (monster_id, len(image_data), len(image_data)))
+    print("finished downloading monster", monster_id)
 
-with open(args.db_config) as f:
-    db_config = json.load(f)
 
-db = pymysql.connect(**db_config, autocommit=True)
-cur = db.cursor()
+async def main():
+    with open(args.db_config) as f:
+        db_config = json.load(f)
+        
+    db = pymysql.connect(**db_config, autocommit=True)
+    cur = db.cursor()
+    file_downloads = []
 
-for file_name in sorted(os.listdir(raw_dir)):
-    if not (match := re.match(r'mons_0*(\d+).bin', file_name)):
-        print('skipping', file_name)
-        continue
+    for file_name in sorted(os.listdir(raw_dir)):
+        if not (match := re.match(r'mons_0*(\d+).bin', file_name)):
+            print('skipping', file_name)
+            continue
 
-    pad_id = match.group(1)
-    final_image_name = '{}.png'.format(pad_id.zfill(5))
-    corrected_file_path = os.path.join(corrected_dir, final_image_name)
+        pad_id = match.group(1)
+        final_image_name = '{}.png'.format(pad_id.zfill(5))
+        corrected_file_path = os.path.join(corrected_dir, final_image_name)
 
-    if os.path.exists(corrected_file_path):
-        print('skipping', corrected_file_path)
-        continue
+        if os.path.exists(corrected_file_path):
+            print('skipping', corrected_file_path)
+            continue
 
-    print('processing', corrected_file_path)
+        print('processing', corrected_file_path)
 
-    gungho_url = GUNGHO_TEMPLATE.format(pad_id)
+        gungho_url = GUNGHO_TEMPLATE.format(pad_id)
 
-    try:
-        size = download_file(gungho_url, corrected_file_path)
-    except Exception as e:
-        print('Failed to download: ', e)
-    else:
-        cur.execute('''INSERT INTO monster_image_sizes (monster_id, hq_png_size, tstamp) 
-                       VALUES (%s, %s, UNIX_TIMESTAMP()) 
-                       ON DUPLICATE KEY 
-                       UPDATE hq_png_size=%s, tstamp=UNIX_TIMESTAMP();''', (int(pad_id), size, size))
+        try:
+            file_downloads.append(download_file(gungho_url, corrected_file_path, int(pad_id), cur))
+        except Exception as e:
+            print('Failed to download: ', e)
 
-cur.close()
-db.close()
-print('done')
+    await asyncio.gather(*file_downloads)
+
+    cur.close()
+    db.close()
+    print('done')
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
